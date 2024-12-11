d88ba97cc3427f507e26e12098ad06b418582a60
Naveen's <naveensiva383@gmail.com>
2024-12-11 11:27:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ project_2_page = st.Page(
     title="Chat Bot",
     icon=":material/smart_toy:",
 )
-project_3_page = st.Page(
+project_3_page = st.page(
     page="views/Web_Automation.py",
     title="Web Automation",
-    icon=":material/service:",
+    icon=":tools:",  # Or choose any other service-related icon
 )
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
