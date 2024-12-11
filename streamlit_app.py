eaf4606c682e1832084b07872c60ad36363920e2
Naveen's <naveensiva383@gmail.com>
2024-12-11 11:37:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ project_2_page = st.Page(
 project_3_page = st.Page(
     page="views/Web_Automation.py",
     title="Web Automation",
-    icon=":material/gear:",  # Or choose any other service-related icon
+    icon=":material/:",  # Or choose any other service-related icon
 )
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
